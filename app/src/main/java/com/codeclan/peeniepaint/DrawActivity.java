798582cfe0e7712b1d3e7fa5a55274b5a02c7063
Rob <robflettcc@gmail.com>
2017-05-15 14:49:46 +0100

    moving to adding features

--- app/src/main/java/com/codeclan/peeniepaint/DrawActivity.java
+++ app/src/main/java/com/codeclan/peeniepaint/DrawActivity.java
@@ -3,14 +3,20 @@ package com.codeclan.peeniepaint;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 
 public class DrawActivity extends AppCompatActivity{
 
+    private DrawView drawView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(new DrawView(this));
+    }
+
+    public void clearCanvas(View v){
+        drawView.clearCanvas();
     }
 }
